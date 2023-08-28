e2a5c155f3457b0d4c6bc06af162bc88252c0891
simo994q <simonkn1@hotmail.com>
2023-08-28 13:35:17 +0200

    borderbottom

--- router-intro/src/components/Navbar/Navbar.jsx
+++ router-intro/src/components/Navbar/Navbar.jsx
@@ -1,21 +1,26 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import style from './Navbar.module.scss'
+import { useEffect } from 'react'
 
 export function Navbar() {
 
     const linksArray = [
-        {url: '/', page: 'Home'},
-        {url: '/about', page: 'About'},
-        {url: '/contact', page: 'Contact'},
-        {url: '/store', page: 'Store'}
+        { url: '/', page: 'Home'},
+        { url: '/about', page: 'About'},
+        { url: '/contact', page: 'Contact'},
+        { url: '/store', page: 'Store'}
     ]
 
     return (
         <div className={style.navbarContainer}>
             <ul>
-                {linksArray.map(item => {
+                {linksArray.map((item, index) => {
                     return (
-                        <li><Link to={item.url} className={style.navbarLink}>{item.page}</Link></li>
+                        <li key={index}><NavLink to={item.url} className={style.navbarLink} style={({ isActive }) => {
+                            return {
+                                borderBottom: isActive ? "2px solid black" : ""
+                            };
+                        }}>{item.page}</NavLink></li>
                     )
                 })}
             </ul>
